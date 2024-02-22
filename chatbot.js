13cc24cebf73026b610ff1f07e0ac4aa4137df22
Afham Irfan <111404292+auth-Afham@users.noreply.github.com>
2024-02-22 11:35:46 +0800

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
-import 'dotenv/config';
 
 const MODEL_NAME = "gemini-pro";
 // Retrieve the API key from the environment variable
